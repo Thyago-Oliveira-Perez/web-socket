35998fb82944735bd8efd9f49460421f3d3a07b2
Thyago-Oliveira-Perez <thyagodoliveiraperez@gmail.com>
2023-05-04 14:30:22 -0300

    feat: init websocket c#

---
+++ c#/webSocket/webSocket/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Net.Sockets;
+using System.Net;
+using System;
+
+namespace webSocket
+{
+    class Server
+    {
+        public static void Main()
+        {
+            TcpListener server = new TcpListener(IPAddress.Parse("127.0.0.1"), 80);
+            server.Start();
+
+            Console.WriteLine("Server has started on 127.0.0.1:80.\nWaiting for a connection...");
+
+            TcpClient client = server.AcceptTcpClient();
+
+            Console.WriteLine("A client connected.");
+        }
+    }
+}
